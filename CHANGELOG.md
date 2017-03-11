65e4892be828db13299ed5db4c949ebcaa47253f
Recca Tsai <recca0120.gmail.com>
2017-03-11 18:45:26 +0800

    update

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+All notable changes to `upload` will be documented in this file
+
+## 1.5.8
+
+- 修正預設 callback
+- 增加 DEMO
--- config/upload.php
+++ config/upload.php
@@ -2,7 +2,7 @@
 
 return [
     'chunks' => storage_path('app/chunks'),
-    'storage' => public_path('/storage/temp'),
+    'storage' => public_path('storage/temp'),
     'domain' => null,
     'path' => 'storage/temp',
 ];
